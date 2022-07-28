420bed91f276fd1b67429f0f7479377596ef0c0f
Pehjos <71464455+pehjos@users.noreply.github.com>
2022-07-28 23:40:33 +0000

    boomo

--- components/About.js
+++ components/About.js
@@ -7,14 +7,14 @@ return (
 <div className="about">
   
 <div className="about__content">
-               <h5>Latest Update</h5>
+<h5>Latest Update</h5>
 <p> 
 As we draw nearer to September and thus the very
  likely launch of the new iPhone 14 lineup, the rumors 
- around Apple's next smartphone are gathering pace. We`&apos;`ve now got 
+ around Apple's next smartphone are gathering pace. We ve now got 
  a rather strong idea of what both the standard iPhone 14 models and the iPhone 14 
  Pro handsets will look like and their cameras and display specs. But one rumors 
- keeps surfacing and it`&apos;`s an odd one 
+ keeps surfacing and its an odd one 
 
 </p>
 </div>
