3554b291e7ef16a8cdfc0fe312e9786f40e0fffa
Pehjos <71464455+pehjos@users.noreply.github.com>
2022-07-28 23:31:52 +0000

    boom

--- components/About.js
+++ components/About.js
@@ -9,7 +9,7 @@ return (
 <div className="about__content">
                <h5>Latest Update</h5>
 <p> 
-As we draw nearer to September and thus the very likely launch of the new iPhone 14 lineup, the rumors around Apple's next smartphone are gathering pace. We’ve now got a rather strong idea of what both the standard iPhone 14 models and the iPhone 14 Pro handsets will look like and their cameras and display specs. But one rumors keeps surfacing and it’s an odd one. 
+As we draw nearer to September and thus the very likely launch of the new iPhone 14 lineup, the rumors around Apple's next smartphone are gathering pace. We&#39ve now got a rather strong idea of what both the standard iPhone 14 models and the iPhone 14 Pro handsets will look like and their cameras and display specs. But one rumors keeps surfacing and it’s an odd one. 
 
 .
 </p>
